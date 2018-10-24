ed69df7a31f185d622eb2b41e9757efca37986ca
Dmitri Antonov <dmitriantonov@Dmitris-MacBook-Pro.local>
2018-10-24 21:20:20 +0300

    added update

---
+++ playground/mongodb-update.js
@@ -0,0 +1,40 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
+  if(err){
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
+  const db = client.db('TodoApp');
+
+  // db.collection('Todos').findOneAndUpdate({
+  //   _id: new ObjectID('5bccb4ace768ae3256687065')
+  // }, {
+  //   $set: {
+  //     completed: true
+  //   }
+  // }, {
+  //   returnOriginal: false
+  // }).then((result) => {
+  //   console.log(result);
+  // });
+
+  db.collection('Users').findOneAndUpdate({
+    _id: new ObjectID('5bd0b41fe768ae32566887be')
+  }, {
+    $set: {
+      name: 'Roman Antonov',
+    },
+      $inc: {
+        age: +2
+    },
+  },
+   {
+    returnOriginal: false
+  
+  }).then((result) => {
+    console.log(result);
+  });
+
+  // client.close();
+})
